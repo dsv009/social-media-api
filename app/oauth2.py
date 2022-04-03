a91c23087273e72de975c9bedfaf0b6639fbaec3
dsv009 <svd95050@gmail.com>
2022-04-03 12:35:48 -0400

    creating post only when the user is login using the token

--- app/oauth2.py
+++ app/oauth2.py
@@ -1,5 +1,10 @@
 from jose import JWTError, jwt
 from datetime import datetime, timedelta
+from . import schemas
+from fastapi import Depends, status, HTTPException
+from fastapi.security import OAuth2PasswordBearer
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl='login')
 
 
 SECRET_KEY = "SHKDJDSDH_akjshj-aksjdshdj@ajshds-sjdhskhjds"
@@ -13,9 +18,27 @@ from datetime import datetime, timedelta
 def create_access_token(data: dict):
     to_encode = data.copy()
 
-    expire = datetime.now() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    expire = datetime.utcnow() +  timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
     to_encode.update({"exp": expire})
 
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
 
     return encoded_jwt
+
+def verify_access_token(token: str, credentials_exception):
+    try:
+        payload =  jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        id: str = payload.get("user_id")
+        if id is None:
+            raise credentials_exception
+        token_data = schemas.TokenData(id=id)
+    except JWTError:
+        raise credentials_exception
+    return token_data
+
+
+def get_current_user(token: str = Depends(oauth2_scheme)):
+    credentials_exception = HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail=f"coud not validate credentials",
+    headers={"www-Authenticate": "Bearer"})
+
+    return verify_access_token(token, credentials_exception)
--- app/routers/post.py
+++ app/routers/post.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI, Response, status, HTTPException, Depends, APIRouter
 from sqlalchemy.orm import Session
 from typing import  List
 from ..database import get_db
-from .. import models, schemas
+from .. import models, schemas, oauth2
 
 router = APIRouter(
     prefix="/posts",
@@ -18,11 +18,13 @@ def get_posts(db: Session = Depends(get_db)):
     return  posts
 
 @router.post('/', status_code=status.HTTP_201_CREATED, response_model=schemas.Post)
-def create_posts(post: schemas.PostCreate, db: Session = Depends(get_db)):
+def create_posts(post: schemas.PostCreate, db: Session = Depends(get_db), user_id: int =
+Depends(oauth2.get_current_user)):
     # cursor.execute('INSERT INTO posts (title, content, published) VALUES (%s, %s, %s) RETURNING *',
     # (post.title, post.content, post.published))
     # new_post = cursor.fetchone()
     # conn.commit()
+    print(user_id)
     new_post = models.Post(**post.dict())
     db.add(new_post)
     db.commit()
